a29a029cadea2857fe9dd834e99d282acef59555
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-05-05 17:23:04 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,28 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        if command.trim() == "exit" {
-            break;
-        } else if command.starts_with("echo") {
-            println!("{}", &command[4..].trim());
-        } else {
-            println!("{}: command not found", command.trim());
+        let commands: Vec<_> = command.split_whitespace().collect();
+
+        match commands.first() {
+            Some(&"exit") => {
+                break;
+            }
+            Some(&"echo") => {
+                println!("{}", commands[1..].join(" "));
+            }
+            Some(&"type") => {
+                if commands.len() > 1 {
+                    if commands[1] == "exit" || commands[1] == "echo" || commands[1] == "type" {
+                        println!("{} is a shell builtin", commands[1]);
+                    } else {
+                        println!("{}: not found", commands[1]);
+                    }
+                }
+            }
+            Some(_) => {
+                println!("{}: command not found", commands[0]);
+            }
+            None => (),
         }
     }
 }
