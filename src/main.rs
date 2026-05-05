6a1e8e1738ed4c9cd840c62185720302017703c8
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-05-05 18:46:23 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs;
 use std::io::{self, Write};
 
 fn main() {
+    let mut executables = HashMap::new();
+
+    if let Ok(paths) = env::var("PATH") {
+        for path in env::split_paths(&paths) {
+            if let Ok(entries) = fs::read_dir(path) {
+                for entry in entries.flatten() {
+                    let executeble_name = entry.file_name().to_string_lossy().to_string();
+                    executables.insert(executeble_name, entry.path());
+                }
+            }
+        }
+    }
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -21,6 +37,16 @@ fn main() {
                 if commands.len() > 1 {
                     if commands[1] == "exit" || commands[1] == "echo" || commands[1] == "type" {
                         println!("{} is a shell builtin", commands[1]);
+                    } else if executables.contains_key(commands[1]) {
+                        println!(
+                            "{} is {}",
+                            commands[1],
+                            executables
+                                .get(commands[1])
+                                .unwrap()
+                                .to_string_lossy()
+                                .to_string()
+                        )
                     } else {
                         println!("{}: not found", commands[1]);
                     }
