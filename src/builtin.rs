dd8b47405b7840bf2a8e5affb5f846ae4ef8dd7e
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-05-05 19:50:22 +0800

    codecrafters submit [skip ci]

--- src/builtin.rs
+++ src/builtin.rs
@@ -1,8 +1,6 @@
-use std::collections::HashMap;
-use std::env;
-use std::fs;
-use std::path::PathBuf;
 use std::process;
+
+use super::exec::build_executables;
 
 pub fn exit_command() {
     process::exit(0);
@@ -36,38 +34,4 @@ pub fn type_command(command: Option<&str>) {
             println!("{}: not found", command);
         }
     }
-}
-
-fn build_executables() -> HashMap<String, PathBuf> {
-    let mut executables = HashMap::new();
-
-    let paths = match env::var("PATH") {
-        Ok(p) => p,
-        Err(_) => return executables,
-    };
-
-    for entry in env::split_paths(&paths)
-        .filter_map(|path| fs::read_dir(path).ok())
-        .flatten()
-        .filter_map(|entry| entry.ok())
-    {
-        let Ok(metadata) = entry.metadata() else {
-            continue;
-        };
-
-        if metadata.is_file() && is_executable(&entry.path()) {
-            let executable_name = entry.file_name().to_string_lossy().to_string();
-            executables.entry(executable_name).or_insert(entry.path());
-        }
-    }
-
-    executables
-}
-
-#[cfg(unix)]
-fn is_executable(path: &std::path::Path) -> bool {
-    use std::os::unix::fs::PermissionsExt;
-    path.metadata()
-        .map(|m| m.permissions().mode() & 0o111 != 0)
-        .unwrap_or(false)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Write};
+use std::process;
 
 mod builtin;
+mod exec;
 
 fn main() {
     loop {
@@ -16,10 +18,32 @@ fn main() {
             Some(&"exit") => builtin::exit_command(),
             Some(&"echo") => builtin::echo_command(&commands[1..].join(" ")),
             Some(&"type") => builtin::type_command(commands.get(1).copied()),
-            Some(_) => {
-                println!("{}: command not found", commands[0]);
-            }
+            Some(command) => exec_external(command, &commands[1..]),
             None => (),
         }
+    }
+}
+
+fn exec_external(command: &str, args: &[&str]) {
+    let executables = exec::build_executables();
+
+    if !executables.contains_key(command) {
+        println!("{}: command not found", command);
+        return;
+    }
+
+    let output = process::Command::new(command)
+        .args(args)
+        .output()
+        .expect("failed to execute process");
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("{}", stdout);
+    }
+
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    if !stderr.is_empty() {
+        eprintln!("{}", stderr);
     }
 }
